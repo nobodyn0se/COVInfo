d18d19d64bd41c0c68f8feedecbe5688c50b83a4
nobodyn0se <48710616+nobodyn0se@users.noreply.github.com>
2020-10-25 11:59:15 +0530

    Added dynamic colors and configured BNB, icon size

--- lib/screens/initpage.dart
+++ lib/screens/initpage.dart
@@ -3,6 +3,7 @@ import 'package:curved_navigation_bar/curved_navigation_bar.dart';
 import 'package:ncov_visual/screens/mainpage.dart';
 import 'package:ncov_visual/screens/countriespage.dart';
 import 'package:ncov_visual/screens/indiapage.dart';
+import 'package:ncov_visual/screen_size.dart';
 
 class InitPage extends StatefulWidget {
   InitPage({Key key}) : super(key: key);
@@ -18,12 +19,26 @@ class _InitPageState extends State<InitPage> {
   //PageView and BottomNavigationBar are independent hence require both
   //initialPage and index properties separately defined
 
+  final List<Color> colorlst = [
+    Colors.orange[100],
+    Colors.lightBlue[200],
+    Colors.green[100]
+  ];
+
+  final List<Color> bgcolor = [
+    //Complements AppBar colors
+    Colors.orange[700],
+    Colors.lightBlue[900],
+    Colors.green[700]
+  ];
+
   List<Widget> _screen = [CountriesPage(), MainPage(), IndiaPage()];
 
   int initIndex = 1; //Center Page - Worldwide displayed initially
 
   @override
   Widget build(BuildContext context) {
+    ScreenSize().init(context);
     return Scaffold(
       body: PageView(
         controller: _pageController,
@@ -31,6 +46,10 @@ class _InitPageState extends State<InitPage> {
         physics: NeverScrollableScrollPhysics(),
       ),
       bottomNavigationBar: CurvedNavigationBar(
+        color: bgcolor[initIndex],
+        backgroundColor: Colors.grey[50], //Scaffold color
+        buttonBackgroundColor: bgcolor[initIndex],
+        height: ScreenSize.blockHeight * 7, //8%
         index: initIndex, //sets the initial index for BottomNavigationBar
         onTap: (int index) {
           setState(() {
@@ -40,10 +59,15 @@ class _InitPageState extends State<InitPage> {
           });
         },
         items: [
-          Icon(Icons.list),
-          Icon(Icons.trending_up),
-          Icon(Icons.location_on)
+          Icon(Icons.list,
+              size: ScreenSize.safeHeight * 4, color: colorlst[initIndex]),
+          Icon(Icons.trending_up,
+              size: ScreenSize.safeHeight * 4, color: colorlst[initIndex]),
+          Icon(Icons.location_on,
+              size: ScreenSize.safeHeight * 4, color: colorlst[initIndex])
         ],
+        animationDuration: Duration(milliseconds: 100),
+        animationCurve: Curves.linearToEaseOut,
       ),
     );
   }
