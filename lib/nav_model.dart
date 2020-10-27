1185a66b611368de1ac0fa201dc63ef2e3babd93
nobodyn0se <48710616+nobodyn0se@users.noreply.github.com>
2020-10-27 23:34:08 +0530

    Added provider

---
+++ lib/nav_model.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import './screens/countriespage.dart';
+import './screens/indiapage.dart';
+import './screens/mainpage.dart';
+
+class NavModel extends ChangeNotifier {
+  int _currentTab = 1;
+  List<Widget> _screen = [CountriesPage(), MainPage(), IndiaPage()];
+
+  set currentTab(int tab) => _currentTab = tab;
+  get currentTab => this._currentTab;
+  get currentScreen => this._screen[this._currentTab]; 
+}
