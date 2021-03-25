b7a95ac42c751b0df1c9e1bbed68623dca6739ec
nobodyn0se <48710616+nobodyn0se@users.noreply.github.com>
2021-03-25 23:03:28 +0530

    update: Made pie chart radius dynamic

--- lib/charts/pieChart.dart
+++ lib/charts/pieChart.dart
@@ -3,7 +3,7 @@ import 'package:fl_chart/fl_chart.dart';
 import 'package:ncov_visual/screen_size.dart';
 
 class GlobalPieChart extends StatelessWidget {
-  final List<int> chartObjects;
+  final List<int> chartObjects; 
 
   GlobalPieChart({this.chartObjects});
 
@@ -12,19 +12,19 @@ class GlobalPieChart extends StatelessWidget {
       PieChartSectionData(
         color: Colors.amber,
         value: chartObjects[0].toDouble(),
-        radius: 20,
+        radius: ScreenSize.safeHeight*2.5,
         title: '',
       ),
       PieChartSectionData(
         color: Colors.red,
         value: chartObjects[1].toDouble(),
-        radius: 20,
+        radius: ScreenSize.safeHeight*2.5,
         title: '',
       ),
       PieChartSectionData(
         color: Colors.green,
         value: chartObjects[2].toDouble(),
-        radius: 20,
+        radius: ScreenSize.safeHeight*2.5,
         title: '',
       ),
     ];
@@ -33,118 +33,118 @@ class GlobalPieChart extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Card(
-        elevation: ScreenSize.safeHeight * 1,
-        child: Padding(
-          padding: EdgeInsets.symmetric(horizontal: ScreenSize.safeWidth * 2),
+      elevation: ScreenSize.safeHeight * 1,
+      child: Padding(
+        padding: EdgeInsets.symmetric(horizontal: ScreenSize.safeWidth * 2),
         child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.start,
           children: [
             Row(
               mainAxisAlignment: MainAxisAlignment.start,
-            children: <Widget>[
-              Expanded(
-                child: AspectRatio(
+              children: <Widget>[
+                Expanded(
+                  child: AspectRatio(
                     aspectRatio: 1.6,
-                  child: Stack(
-                    alignment: Alignment.center,
-                    children: <Widget>[
-                      PieChart(
-                        PieChartData(
-                            startDegreeOffset: 270,
-                            centerSpaceColor: Colors.transparent,
-                            borderData: FlBorderData(
-                              show: false,
-                            ),
-                            sectionsSpace: 0,
+                    child: Stack(
+                      alignment: Alignment.center,
+                      children: <Widget>[
+                        PieChart(
+                          PieChartData(
+                              startDegreeOffset: 270,
+                              centerSpaceColor: Colors.transparent,
+                              borderData: FlBorderData(
+                                show: false,
+                              ),
+                              sectionsSpace: 0,
                               centerSpaceRadius: ScreenSize.safeHeight * 5.5,
-                            sections: showingSections()),
-                      ),
-                      Text(
-                        'Total\n${(chartObjects[3] * 0.000001).toStringAsFixed(2)}M',
-                        textAlign: TextAlign.center,
-                        style: TextStyle(
-                          fontSize: ScreenSize.safeHeight * 1.9,
+                              sections: showingSections()),
                         ),
-                      ),
-                    ],
+                        Text(
+                          'Total\n${(chartObjects[3] * 0.000001).toStringAsFixed(2)}M',
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                            fontSize: ScreenSize.safeHeight * 1.9,
+                          ),
+                        ),
+                      ],
+                    ),
                   ),
                 ),
-              ),
-              Column(
-                mainAxisSize: MainAxisSize.max,
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  Row(
-                    children: [
-                      Container(
-                        width: ScreenSize.safeHeight * 2,
-                        height: ScreenSize.safeHeight * 2,
-                        decoration: BoxDecoration(
-                          border: Border.all(
-                              color: Colors.black,
-                              width: ScreenSize.safeWidth * 0.3),
-                          shape: BoxShape.circle,
-                          color: Colors.amber,
+                Column(
+                  mainAxisSize: MainAxisSize.max,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Row(
+                      children: [
+                        Container(
+                          width: ScreenSize.safeHeight * 2,
+                          height: ScreenSize.safeHeight * 2,
+                          decoration: BoxDecoration(
+                            border: Border.all(
+                                color: Colors.black,
+                                width: ScreenSize.safeWidth * 0.3),
+                            shape: BoxShape.circle,
+                            color: Colors.amber,
+                          ),
                         ),
-                      ),
-                      SizedBox(
-                        width: ScreenSize.safeWidth * 1,
-                      ),
-                      Text(
-                          'Active: ${(chartObjects[0] * 0.000001).toStringAsFixed(2)}M'),
-                    ],
-                  ),
-                  SizedBox(
-                    height: ScreenSize.safeHeight * 1,
-                  ),
-                  Row(
-                    children: [
-                      Container(
-                        width: ScreenSize.safeHeight * 2, //1 unit is 7
-                        height: ScreenSize.safeHeight * 2,
-                        decoration: BoxDecoration(
-                          border: Border.all(
-                              color: Colors.black,
-                              width: ScreenSize.safeWidth * 0.3),
-                          shape: BoxShape.circle,
-                          color: Colors.red,
+                        SizedBox(
+                          width: ScreenSize.safeWidth * 1,
                         ),
-                      ),
-                      SizedBox(
-                        width: ScreenSize.safeWidth * 1,
-                      ),
-                      Text(
-                          'Deaths: ${(chartObjects[1] * 0.000001).toStringAsFixed(2)}M'),
-                    ],
-                  ),
-                  SizedBox(
-                    height: ScreenSize.safeHeight * 1,
-                  ),
-                  Row(
-                    children: [
-                      Container(
-                        width: ScreenSize.safeHeight * 2,
-                        height: ScreenSize.safeHeight * 2,
-                        decoration: BoxDecoration(
-                          border: Border.all(
-                              color: Colors.black,
-                              width: ScreenSize.safeWidth * 0.3),
-                          shape: BoxShape.circle,
-                          color: Colors.green,
+                        Text(
+                            'Active: ${(chartObjects[0] * 0.000001).toStringAsFixed(2)}M'),
+                      ],
+                    ),
+                    SizedBox(
+                      height: ScreenSize.safeHeight * 1,
+                    ),
+                    Row(
+                      children: [
+                        Container(
+                          width: ScreenSize.safeHeight * 2, //1 unit is 7
+                          height: ScreenSize.safeHeight * 2,
+                          decoration: BoxDecoration(
+                            border: Border.all(
+                                color: Colors.black,
+                                width: ScreenSize.safeWidth * 0.3),
+                            shape: BoxShape.circle,
+                            color: Colors.red,
+                          ),
                         ),
-                      ),
-                      SizedBox(
-                        width: ScreenSize.safeWidth * 1,
-                      ),
-                      Text(
-                          'Recovered: ${(chartObjects[2] * 0.000001).toStringAsFixed(2)}M'),
-                    ],
-                  ),
-                ],
-              ),
-            ],
-          ),
+                        SizedBox(
+                          width: ScreenSize.safeWidth * 1,
+                        ),
+                        Text(
+                            'Deaths: ${(chartObjects[1] * 0.000001).toStringAsFixed(2)}M'),
+                      ],
+                    ),
+                    SizedBox(
+                      height: ScreenSize.safeHeight * 1,
+                    ),
+                    Row(
+                      children: [
+                        Container(
+                          width: ScreenSize.safeHeight * 2,
+                          height: ScreenSize.safeHeight * 2,
+                          decoration: BoxDecoration(
+                            border: Border.all(
+                                color: Colors.black,
+                                width: ScreenSize.safeWidth * 0.3),
+                            shape: BoxShape.circle,
+                            color: Colors.green,
+                          ),
+                        ),
+                        SizedBox(
+                          width: ScreenSize.safeWidth * 1,
+                        ),
+                        Text(
+                            'Recovered: ${(chartObjects[2] * 0.000001).toStringAsFixed(2)}M'),
+                      ],
+                    ),
+                  ],
+                ),
+              ],
+            ),
             Divider(
               color: Colors.black38,
               indent: ScreenSize.safeWidth*2,
